06c944447fa80024314ff55e521dbdf5f4d63bef
Erik Harper <eharperdicianno@esri.com>
2022-06-21 11:00:08 -0700

    Committing WIP changes while I get feedback from Slack channel

--- test/radio-button.test.js
+++ test/radio-button.test.js
@@ -1,16 +1,35 @@
 import { html, fixture } from "@open-wc/testing";
 import { visualDiff } from "@web/test-runner-visual-regression";
 
-// import "../dist/calcite/calcite.css";
 import "../dist/components/calcite-label.js";
 import "../dist/components/calcite-radio-button.js";
 
 it("small labeled radio-button", async () => {
   const el = await fixture(html`
-    <calcite-label for="stencil-def-s" layout="inline" scale="s">
-      <calcite-radio-button id="stencil-def-s" name="def-s" scale="s" value="stencil-def-s"></calcite-radio-button>
-      Stencil
-    </calcite-label>
+    <!DOCTYPE html>
+    <html lang="en">
+      <head> </head>
+      <body>
+        <p>Paragraph</p>
+      </body>
+    </html>
   `);
   await visualDiff(el, "small labeled radio-button");
 });
+// it("small labeled radio-button", async () => {
+//   const el = await fixture(html`
+//     <!DOCTYPE html>
+//     <html lang="en">
+//       <head>
+//         <link rel="stylesheet" href="dist/calcite/calcite.css" />
+//       </head>
+//       <body>
+//         <calcite-label for="stencil-def-s" layout="inline" scale="s">
+//           <calcite-radio-button id="stencil-def-s" name="def-s" scale="s" value="stencil-def-s"></calcite-radio-button>
+//           Stencil
+//         </calcite-label>
+//       </body>
+//     </html>
+//   `);
+//   await visualDiff(el, "small labeled radio-button");
+// });
