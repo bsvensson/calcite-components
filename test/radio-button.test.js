eaedbe064e43b808ca980b4209e35e7a9a5939ba
Erik Harper <eharperdicianno@esri.com>
2022-06-28 15:09:14 -0700

    got WTR working!

--- test/radio-button.test.js
+++ test/radio-button.test.js
@@ -4,32 +4,33 @@ import { visualDiff } from "@web/test-runner-visual-regression";
 import "../dist/components/calcite-label.js";
 import "../dist/components/calcite-radio-button.js";
 
+it("paragraph", async () => {
+  const el = await fixture(html` <p>Paragraph</p> `);
+  await visualDiff(el, "paragraph");
+});
+
 it("small labeled radio-button", async () => {
   const el = await fixture(html`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head> </head>
-      <body>
-        <p>Paragraph</p>
-      </body>
-    </html>
+    <calcite-label for="stencil-def-s" layout="inline" scale="s">
+      <calcite-radio-button id="stencil-def-s" name="def-s" scale="s" value="stencil-def-s"></calcite-radio-button>
+      Stencil
+    </calcite-label>
   `);
   await visualDiff(el, "small labeled radio-button");
 });
-// it("small labeled radio-button", async () => {
-//   const el = await fixture(html`
-//     <!DOCTYPE html>
-//     <html lang="en">
-//       <head>
-//         <link rel="stylesheet" href="dist/calcite/calcite.css" />
-//       </head>
-//       <body>
-//         <calcite-label for="stencil-def-s" layout="inline" scale="s">
-//           <calcite-radio-button id="stencil-def-s" name="def-s" scale="s" value="stencil-def-s"></calcite-radio-button>
-//           Stencil
-//         </calcite-label>
-//       </body>
-//     </html>
-//   `);
-//   await visualDiff(el, "small labeled radio-button");
-// });
+
+it("small labeled checked radio-button", async () => {
+  const el = await fixture(html`
+    <calcite-label for="stencil-def-s" layout="inline" scale="s">
+      <calcite-radio-button
+        checked
+        id="stencil-def-s"
+        name="def-s"
+        scale="s"
+        value="stencil-def-s"
+      ></calcite-radio-button>
+      React
+    </calcite-label>
+  `);
+  await visualDiff(el, "small labeled checked radio-button");
+});
--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,5 +1,17 @@
 import { visualRegressionPlugin } from "@web/test-runner-visual-regression/plugin";
 
 export default {
-  plugins: [visualRegressionPlugin()]
+  plugins: [
+    visualRegressionPlugin({
+      update: process.argv.includes("--update-visual-baseline")
+    })
+  ],
+  testRunnerHtml: (testFramework) =>
+    `<html>
+      <head><link rel="stylesheet" href="dist/calcite/calcite.css" /></head>
+      <body>
+        <script>window.process = { env: { NODE_ENV: "development" } }</script>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`
 };
